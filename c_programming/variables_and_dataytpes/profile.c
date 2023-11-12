43181d3ff54e6d366a332de164d9d05c98e4582c
hunterxcobby <solomonsefah13@gmail.com>
2023-11-12 20:46:49 +0000

    check for profile of students

---
+++ c_programming/variables_and_dataytpes/profile.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char *studentName = "Kelvin Nyantakyi Owusu";
+	char *residence = "Adjima";
+	int age = 21;
+	float height = 12.7;
+	char gender = 'M';
+
+	printf("Student's name %s\n", studentName);
+	printf("Residence %s\n", residence);
+	printf("Age is %d\n", age);
+	printf("Height is %.1f\n", height);
+	printf("Gender is %c\n", gender);
+
+    
+
+	return(0);
+}
