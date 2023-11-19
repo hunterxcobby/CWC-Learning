14bc9c0310a2e1514ddc421809089d2bf6038f19
hunterxcobby <solomonsefah13@gmail.com>
2023-11-19 17:55:56 +0000

    continue this program and submit

--- c_programming/variables_and_dataytpes/sprofile.c
+++ c_programming/variables_and_dataytpes/sprofile.c
@@ -3,12 +3,18 @@
 int main(void)
 {
     char student_Name[50];
+    int age;
+    char residence[50];
 
 	printf("This is a school registration form\n");
 
     printf("Name:");
-    scanf("%[^\n]%*c", student_Name);
+    scanf("%[^\n]%*c", student_Name);/*  Escape sequence*/
     printf("student name is %s\n", student_Name);
+
+    printf("Age:");
+    scanf("%d", &age);
+    printf("Age is %d\n", age);
 
 	return(0);
 }
