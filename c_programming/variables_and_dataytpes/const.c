76908103b27c2185ca53199084c9746fc91a986e
hunterxcobby <solomonsefah13@gmail.com>
2023-11-19 19:46:07 +0000

    constant variables

---
+++ c_programming/variables_and_dataytpes/const.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+    /* declare a constant variable */
+    const int accountNumber = 507323;
+    const long int id_number = 88484862970;
+    const int birthyear = 1889;
+    const float pi = 3.14;
+
+    printf("%d\n", accountNumber);
+    printf("%ld\n", id_number);
+    printf("%d\n", birthyear);
+    printf("%.2f\n", pi);
+
+
+
+    return (0);
+
+}
