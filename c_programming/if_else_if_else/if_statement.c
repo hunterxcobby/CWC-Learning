3da217ff085deea802e98e7518546a8bed371ca9
hunterxcobby <solomonsefah13@gmail.com>
2023-11-26 17:56:58 +0000

    if statement

---
+++ c_programming/if_else_if_else/if_statement.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main (void)
+{
+    int age = 11;
+    int adult = 18;
+
+    if (age >= adult)
+    {
+       printf("loading website...\n");
+       /* The rest of the code*/
+    }
+    
+
+    return(0);
+}
