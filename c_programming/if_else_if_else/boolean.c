11ee846c4bb71f7b38ec27ac8c5ff417fcf3b85f
hunterxcobby <solomonsefah13@gmail.com>
2023-11-26 20:02:22 +0000

    boolean

---
+++ c_programming/if_else_if_else/boolean.c
@@ -0,0 +1,21 @@
+#include <stdbool.h>
+#include <stdio.h>
+
+int main (void)
+{
+    bool isDianaPresent = true;
+    bool isAkwesiPresent = false;
+
+    if (!isDianaPresent)
+    {
+        printf("Tell her to mark the books\n");
+    }
+    else
+    {
+        printf("Tell me Diana is not present.\n");
+    }
+
+    
+
+    return(0);
+}
