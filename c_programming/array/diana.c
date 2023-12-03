b87767c468814982707e3548bae12a6f25f63b2d
hunterxcobby <solomonsefah13@gmail.com>
2023-12-03 21:45:43 +0000

    first array

--- c_programming/array/diana.c
+++ c_programming/array/diana.c
@@ -2,10 +2,20 @@
 
 int main(void)
 {
-    int numbers[] = {20, 45, 59, 70} ;
-    char *string = "Akwesi";
+    int numbers[] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110};
 
-    printf("%d\n", numbers[0]);
+    printf("%d\n", numbers[0]); /* index 0*/
+    printf("%d\n", numbers[1]);
+    printf("%d\n", numbers[2]);
+    printf("%d\n", numbers[3]);
+    printf("%d\n", numbers[4]);
+    printf("%d\n", numbers[5]);
+    printf("%d\n", numbers[6]);
+    printf("%d\n", numbers[7]);
+    printf("%d\n", numbers[8]);
+    printf("%d\n", numbers[9]);
+    printf("%d\n", numbers[10]);/* index 9*/
+
 
     return(0);
 }
