4731ed86d09b296e8d46d94754ce511bf0f9b319
hunterxcobby <solomonsefah13@gmail.com>
2023-12-03 21:47:10 +0000

    how to access an array

---
+++ c_programming/array/acces_array.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int idx;
+    int numbers[] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+
+
+
+    for (idx = 0; idx < 4; idx++)
+    {
+        printf("%d\n", numbers[idx]);
+    }
+    
+
+    return(0);
+}
