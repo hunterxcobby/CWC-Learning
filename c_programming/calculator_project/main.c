bbebe40150591487484bb7d8ddaaf71747bb12d2
hunterxcobby <solomonsefah13@gmail.com>
2023-12-28 19:39:32 +0000

    update file

---
+++ c_programming/calculator_project/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int num1;
+    int num2;
+
+
+    printf(" Calculator Application\n");
+    printf("<----------------------->\n");
+
+    printf("Enter two numbers\n");
+    scanf("%d %d", &num1, &num2);
+
+    /*printf("Two numbers are %d %d", num1, num2);*/
+
+    return (0);
+}
