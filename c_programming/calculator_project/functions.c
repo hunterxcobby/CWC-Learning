9cf27a6ad38bd0c9c10e4dd5ee7bffc64dae8bf1
hunterxcobby <solomonsefah13@gmail.com>
2023-12-30 18:07:38 +0000

    function definitons

---
+++ c_programming/calculator_project/functions.c
@@ -0,0 +1,37 @@
+
+
+/* Addition*/
+int add(int num1, int num2)
+{
+    int result;
+    result = num1 + num2;
+
+    return (result);
+}
+
+/* Multiplication*/
+int mul(int num1, int num2)
+{
+    int result;
+    result = num1 * num2;
+
+    return(result);
+}
+
+int div(int num1, int num2)
+{
+    int result;
+
+    result = num1 / num2;
+
+    return(result);
+}
+
+int sub(int num1, int num2)
+{
+    int result;
+
+    result = num1 - num2;
+
+    return (result);
+}
