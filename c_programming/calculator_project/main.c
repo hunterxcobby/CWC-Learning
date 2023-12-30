29b2cef814e415cef43e71dee3494f8574310254
hunterxcobby <solomonsefah13@gmail.com>
2023-12-30 17:42:51 +0000

    update file

--- c_programming/calculator_project/main.c
+++ c_programming/calculator_project/main.c
@@ -13,7 +13,7 @@ int main(void)
     int choice;
     int result;
     char terminate[5];
-    char *string[] = {"yes", "y", "YES"};
+    /*char *string[] = {"yes", "y", "YES"};*/
 
 
 
@@ -38,12 +38,12 @@ do {
     {
         case 1:
             /*printf("You chose 1\n");*/
-            result = add(num1, num2);
+            result = calculate(num1, num2, add);
             printf("Sum is %d\n", result);
             break;
         case 2:
             /*printf("You chose 2\n");*/
-            result = sub(num1, num2);
+            result = calculate(num1, num2, sub);
             printf("Difference is %d\n", result);
             break;
         case 3:
@@ -55,13 +55,13 @@ do {
             }
             else
             {
-                result = div(num1, num2);
+                result = calculate(num1, num2, div);
                 printf("Result is %d\n", result);
             }
             break;
         case 4:
             /*printf("You chose 4\n");*/
-            result = mul(num1, num2);
+            result = calculate(num1, num2, mul);
             printf("Product is %d\n", result);
             break;
         default:
@@ -75,7 +75,7 @@ do {
 
     printf("You chose %s\n", terminate);
 
-} while (strcmp(terminate, string[0]) == 0);
+} while (strcmp(terminate, "yes") == 0);
 
     return (0);
 }
@@ -90,7 +90,7 @@ int add(int num1, int num2)
     return (result);
 }
 
-/* MUltiplication*/
+/* Multiplication*/
 int mul(int num1, int num2)
 {
     int result;
@@ -115,4 +115,9 @@ int sub(int num1, int num2)
     result = num1 - num2;
 
     return (result);
+}
+
+int calculate(int num1, int num2, int *operation(int num1, int num2))
+{
+    return (operation(num1, num2));
 }
