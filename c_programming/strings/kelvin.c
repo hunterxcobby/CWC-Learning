8b21e5cb3790a99a74d512b901ab1ad4748f4692
hunterxcobby <solomonsefah13@gmail.com>
2024-01-20 21:58:51 +0000

    cwc class:kelvins assignment

---
+++ c_programming/strings/kelvin.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+
+char *concatenateStrings(char name[], char name1[]);
+
+int main(void)
+{
+
+    char name[25];
+    char name1[25];
+    char *box;
+
+    printf("Enter the first string:");
+    scanf ("%s",name);
+
+    printf("Enter the second string:");
+    scanf ("%s",name1);
+
+    box = concatenateStrings(name, name1);
+    printf("%s\n", box);
+
+    return(0);
+}
+
+
+char *concatenateStrings(char name[], char name1[])
+{
+    char *result;
+    /** 
+     * concatenate name1 to name
+     *results is stored in name
+     */
+    result = strcat(name, name1);
+
+    return (result);
+}
