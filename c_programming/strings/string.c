e8ab10cca073b1bf072e99efeb9633626951d069
hunterxcobby <solomonsefah13@gmail.com>
2024-01-20 21:58:37 +0000

    cwc class:string

--- c_programming/strings/string.c
+++ c_programming/strings/string.c
@@ -1,13 +1,36 @@
 #include <stdio.h>
 #include <string.h>
 
+char *concatenateStrings(char name[], char name1[]);
+
 int main(void)
 {
 
-    char name[] = "Diana";
+    char name[25];
+    char name1[25];
+    char box[50];
 
-    printf("the size is %lu characters\n", sizeof(name));
-    /* there is a null character*/
-    
+    printf("Enter the first string:");
+    scanf ("%s",name);
+
+    printf("Enter the second string:");
+    scanf ("%s",name1);
+
+    box = concatenateStrings(name, name1);
+    printf("%s", box);
+
     return(0);
+}
+
+
+char *concatenateStrings(char name[], char name1[])
+{
+    char *result;
+    /** 
+     * concatenate name1 to name
+     *results is stored in name
+     */
+    result = strcat(name, name1);
+
+    return (result);
 }
