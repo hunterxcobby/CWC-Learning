7c586fa3e81b55ec19325b31874bb4b7963406c5
hunterxcobby <solomonsefah13@gmail.com>
2024-01-31 22:15:23 +0000

    a new file

--- c_programming/1-debugging/printf.c
+++ c_programming/1-debugging/printf.c
@@ -1,10 +1,57 @@
-#include <stdio.h> 
+#include <stdio.h>
 
-int main(void)
+int main(void)/*Entry point of the program*/
+
 {
-    /* To print to the screen*/
-    printf("Hello, World\n");
+	char Student_Name[50];
+	char Residence[50];
+	int Age;
+	float Height;
+	char Gender;
 
-    return 0; 
-}
+	/*Adjima's school registration form*/
+	
+	
+	printf("This is a school registration form\n");
+	
+	/*takes and prints tye name of the student*/
+	printf("NAME:");
+	scanf("%[^\n]%*c", Student_Name);
 
+
+	/*takes and prints the residence of the student*/
+	printf("RESIDENCE:");
+	scanf("%[^\n]%*c", Residence);
+	
+
+
+	/*takes the age of the student*/
+	printf("Age:");
+	scanf("%d", &Age);
+	
+
+	/*takes and prints the height of the student in inches*/
+	printf("Height:");
+	scanf("%f", &Height);
+
+	/*takes and prints the gender of the student*/
+	printf("Gender:");
+	scanf(" %c", &Gender);
+	
+
+	printf("Student's name is %s\n", Student_Name);
+	printf("Student's residence is %s\n", Residence);
+	printf("Age of student is %d\n", Age);
+	printf("Height(in inches)of student is %.2f\n", Height);
+	printf("Gender of student is %c\n", Gender);
+
+	return 0;
+
+}	
+
+
+
+
+
+
+		
